221842d907be5850c4795fc99f5d24fa6f934c4a
Lukas Hufnagel-Ben-Hadar <weblorkes@gmail.com>
2019-08-16 23:29:43 +0200

    Added rutimentary collisions. To do: angular force transmission

--- ball.js
+++ ball.js
@@ -6,14 +6,23 @@ class Ball {
 	}
 	move() {
 		// check for collisions with the canvas boundries 
-		if (this.position.x <= 0 + this.size/2 || this.position.x >= width - this.size/2)
-			{ this.velocity.x *= -1; } 
-	  	if (this.position.y <= 0 + this.size/2 || this.position.y >= height - this.size/2) 			{ this.velocity.y *= -1; } 
+		if (this.position.x <= 0 + this.size/2 || this.position.x >= width - this.size/2)	{
+			this.velocity.x *= -1; 
+		} 
+	  if (this.position.y <= 0 + this.size/2 || this.position.y >= height - this.size/2) {
+			this.velocity.y *= -1;
+		} 
 		
 		// move the ball to it's new position by adding velocity to position	
 		this.position.add(this.velocity);
 	}
 	show() {
   		ellipse(this.position.x, this.position.y, this.size);
+	}
+
+	isColliding(other) {
+		let d = this.position.dist(other.position);
+		// size equals to the diameter but since position is at the center of the ellipse we need to divive by 2 to get the circles radii
+		return (d < this.size / 2 + other.size / 2);
 	}
 }
--- sketch.js
+++ sketch.js
@@ -1,21 +1,29 @@
-var balls = [];
+let balls = [];
+// let myBall = new Ball(createVector(width/2, height/2), createVector(0,0), 80); 
 
 function setup() {
   // put setup code here
 	createCanvas(800,800);
-	for (i = 0; i < 15; i++) {
-		let size = 40;	
-		let position = createVector(size + (50 * i), size + 5);
-		let velocity = createVector(3, 6);
+	for (let i = 0; i < 8; i++) {
+		let size = random(40, 120);	
+		let position = createVector(size + random(0,width - size), size + random(0, height - size));
+		let velocity = createVector(random(-10,10), random(-10,10));
 		balls.push(new Ball(position, velocity, size));
 	}
-}
+ } 
 
 function draw() {
   // put drawing code here
  	background(51);
-  
-	for (i = 0; i < balls.length; i++) {
+	for (let i = 0; i < balls.length; i++) {
+		for (let j = i + 1; j < balls.length; j++) {
+			//console.log(i, j);
+			//console.log(balls[i].isColliding(balls[j]));
+			if (balls[i].isColliding(balls[j])) {
+				balls[i].velocity.mult(-1);
+				balls[j].velocity.mult(-1);
+			}
+		}
 		balls[i].move();
 		balls[i].show();
 	}
