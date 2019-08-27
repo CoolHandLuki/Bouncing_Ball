93417f196af38a9d3c05a70c17897c46303b2f3c
Lukas Hufnagel-Ben-Hadar <weblorkes@gmail.com>
2019-08-27 22:31:01 +0200

    Moved edge checking into own function

--- ball.js
+++ ball.js
@@ -5,6 +5,12 @@ class Ball {
 		this.size = size;
 	}
 	move() {
+		checkEdges();
+		// move the ball to it's new position by adding velocity to position	
+		this.position.add(this.velocity);
+	}
+
+	checkEdges() {
 		// check for collisions with the canvas boundries 
 		if (this.position.x <= 0 + this.size/2 || this.position.x >= width - this.size/2)	{
 			this.velocity.x *= -1; 
@@ -12,10 +18,8 @@ class Ball {
 	  if (this.position.y <= 0 + this.size/2 || this.position.y >= height - this.size/2) {
 			this.velocity.y *= -1;
 		} 
-		
-		// move the ball to it's new position by adding velocity to position	
-		this.position.add(this.velocity);
 	}
+	
 	show() {
   		ellipse(this.position.x, this.position.y, this.size);
 	}
