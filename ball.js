cf8981135d0a6edeeda9b361e01db4bf9fab25df
Lukas Hufnagel-Ben-Hadar <weblorkes@gmail.com>
2019-09-22 17:44:22 +0200

    Bug fixes with collisions

--- ball.js
+++ ball.js
@@ -1,42 +1,62 @@
 class Ball {
-	constructor(position, velocity, size,) {
+	constructor(position, _velocity, _size,) {
 		this.position = position;
-		this.velocity = velocity;
+		this.velocity = _velocity;
 		this.acceleration = createVector(0,0);
-		this.size = size;
-		this.mass = size; // setting mass equal to size for now
+		this.size = floor(_size);
+		this.mass = _size; // setting mass equal to size for now
+		this.color = [255,255,255]; 
 	}
 
 	applyForce(force) {
 		this.acceleration.add(force);	
 	}
 
-	checkEdges() {
+	checkEdges(_friction) {
 		// check for collisions with the canvas boundries 
-		if (this.position.x <= 0 + this.size/2 || this.position.x >= width - this.size/2)	{
-			this.velocity.x *= -1; 
-		} 
-	  if (this.position.y <= 0 + this.size/2 || this.position.y >= height - this.size/2) {
-			this.velocity.y *= -1;
-		} 
+		if (this.position.x < 0 + (this.size / 2))	{
+			this.position.x = 0 + (this.size / 2);
+			this.velocity.x *= -_friction; 
+		} else if (this.position.x > width - this.size/2) {
+			this.position.x = width - (this.size / 2);
+			this.velocity.x *= -_friction;
+		}
+	  if (this.position.y < 0 + this.size / 2) {
+			this.position.y = 0 + (this.size / 2);
+			this.velocity.y *= -_friction;
+		} else if (this.position.y > height - this.size / 2) {
+			this.position.y = height - (this.size / 2);
+			this.velocity.y *= -_friction;
+			// let invertAcceleration = createVector(0, this.acceleration.y *= -1);
+			// this.applyForce(invertAccelertion);
+		}
 	}
 	
-	move() {
+	move(_friction) {
 		// move the ball to it's new position by adding velocity to position	
-		this.checkEdges();
 		this.velocity.add(this.acceleration);	
 		this.position.add(this.velocity);
-		this.acceleration = createVector(0,0);
+		this.floorVector(this.position); 
+		this.acceleration *= 0;
+		this.checkEdges(_friction);
 	}
 
-
 	show() {
+			noStroke();
+			fill(this.color);
   		ellipse(this.position.x, this.position.y, this.size);
+			stroke(255,0,0);
+			line(this.position.x, this.position.y, this.position.x, this.position.y + this.size / 2);
 	}
 
 	isColliding(other) {
 		let d = this.position.dist(other.position);
 		// size equals to the diameter but since position is at the center of the ellipse we need to divive by 2 to get the circles radii
 		return (d < this.size / 2 + other.size / 2);
+	}
+
+	floorVector(_vector) {
+		_vector.x = floor(_vector.x);
+		_vector.y = floor(_vector.y);
 	}
 }
--- sketch.js
+++ sketch.js
@@ -1,17 +1,19 @@
 let balls = [];
 let gravity;
-// let myBall = new Ball(createVector(width/2, height/2), createVector(0,0), 80); 
+let wind;
+let friction;
 
 function setup() {
   // put setup code here
-	createCanvas(800,800);
-	
-	gravity = createVector(0,0.5);
-	
-	for (let i = 0; i < 8; i++) {
-		let size = random(40, 120);	
+	createCanvas(600,600);
+	gravity = createVector(0, 01);
+	friction = 0.9;
+
+
+	for (let i = 0; i < 2; i++) {
+		let size = random(40, 80);	
 		let position = createVector(size + random(0,width - size), size + random(0, height - size));
-		let velocity = createVector(random(-10,10), random(-10,10));
+		let velocity = createVector(random(-5,5), random(-5,5));
 		balls.push(new Ball(position, velocity, size));
 	}
  } 
@@ -19,17 +21,31 @@ function setup() {
 function draw() {
   // put drawing code here
  	background(51);
+
 	for (let i = 0; i < balls.length; i++) {
 		for (let j = i + 1; j < balls.length; j++) {
-			//console.log(i, j);
-			//console.log(balls[i].isColliding(balls[j]));
-			if (balls[i].isColliding(balls[j])) {
-				balls[i].velocity.mult(-1);
-				balls[j].velocity.mult(-1);
-			}
+			 if (balls[i].isColliding(balls[j])) {
+			 	balls[i].velocity.mult(-friction);
+			 	balls[j].velocity.mult(-friction);
+			 }
 		}
-		balls[i].applyForce(gravity);
-		balls[i].move();
+		if (balls[i].position.y > (height - (balls[i].size) / 2) + 5) {
+			balls[i].color = [255,255,0];
+		} else {
+			balls[i].color = [255,255,255];
+			// balls[i].applyForce(gravity);
+			balls[i].acceleration = gravity;
+		}
+	//	if (floor(balls[i].position.y) == 300) {
+	//		console.log('Ball is at: ', floor(balls[i].position.y));
+	//		balls[i].setColor([0,255,0]);
+	//		balls[i].applyForce(gravity);
+	//		}
+	//	else {
+	//		balls[i].setColor([125, 125, 0]);
+	//	}
+		// console.log(floor(balls[i].position.y));
+		balls[i].move(friction);
 		balls[i].show();
 	}
 }
